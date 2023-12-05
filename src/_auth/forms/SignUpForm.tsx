23516415e935809dc0916d624c32014f57ea43ce
_davidoskiii <davide.mascherin9@gmail.com>
2023-12-05 20:48:25 +0100

    Fist commit, added all the neccessary tools for the front-end

---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const SignInForm = () => {
+    return (
+        <div>
+            <Button>Palle</Button>
+        </div>
+    )
+}
+
+export default SignInForm
